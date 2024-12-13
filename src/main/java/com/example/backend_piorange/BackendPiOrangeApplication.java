adc8a4b00cfe161a0401ac3e8d2cba623ac9a11c
Saloua_jbabri <salouajb.44@gmail.com>
2024-12-13 08:40:48 +0100

    projj

---
+++ src/main/java/com/example/backend_piorange/BackendPiOrangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_piorange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPiOrangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendPiOrangeApplication.class, args);
+    }
+
+}
